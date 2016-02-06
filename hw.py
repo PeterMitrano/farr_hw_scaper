46b84953f7e6f69a53801cfb4f25cd2a7f43cafb
Peter_Mitrano <mitranopeter@gmail.com>
2016-02-06 14:50:13 -0500

    using variable for last index
    also print out which hw it is

--- hw.py
+++ hw.py
@@ -7,14 +7,15 @@ pages = requests.get('http://www.math.wpi.edu/Course_Materials/MA2071C16/Hw/hwk.
 
 soup = BeautifulSoup(pages.text,'html.parser')
 current_hw_num = len(soup.findAll('a'))-1
-
+print current_hw_num
 page = requests.get('http://www.math.wpi.edu/Course_Materials/MA2071C16/Hw/hw'+str(current_hw_num)+'.html')
 soup = BeautifulSoup(page.text,'html.parser')
 lines = soup.find('pre').text.split('\n')
 
 for lin_num in range(3,len(lines)-1):
     section = lines[lin_num].split(" ")[0]
-    problems = lines[lin_num].split(" ")[27].split(',')
+    last = len(lines[lin_num].split(" "))-1
+    problems = lines[lin_num].split(" ")[last].split(',')
     print section, ":  ", ' '.join([p.encode('ascii', 'ignore') for p in problems])
 
 
